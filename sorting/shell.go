6f33f8a0a411daf9e0d53858c3b6d0f9aafa7533
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-05-24 11:11:21 +0300

    Add 'Shell' sort

---
+++ sorting/shell.go
@@ -0,0 +1,20 @@
+package sort
+
+import builtinSort "sort"
+
+// Shell sorts target with "Shell sort" algorithm.
+func Shell(target builtinSort.Interface) {
+	len := target.Len()
+	h := 1
+	for h < len/3 {
+		h = 3*h + 1
+	}
+	for h >= 1 {
+		for i := h; i < len; i++ {
+			for j := i; j >= h && target.Less(j, j-h); j -= h {
+				target.Swap(j, j-h)
+			}
+		}
+		h /= 3
+	}
+}
