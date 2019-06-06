ea3c7ce6f98fd6792e5e09ad6f6d8ec981a021d3
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-06-06 22:30:11 +0300

    Implement union find

--- graph/minimum_spanning_tree_kruskal.go
+++ graph/minimum_spanning_tree_kruskal.go
@@ -1,5 +1,52 @@
 package graph
 
+type unionFind struct {
+	parent []int
+	rank   []byte
+}
+
+func (uf *unionFind) find(p int) int {
+	i := p
+	parent := uf.parent
+	for i != parent[i] {
+		i = parent[i]
+	}
+	return i
+}
+
+func (uf *unionFind) union(p, q int) {
+	pRoot, qRoot := uf.find(p), uf.find(q)
+	if pRoot == qRoot {
+		return
+	}
+	pRank, qRank := uf.rank[pRoot], uf.rank[qRoot]
+	if pRank < qRank {
+		uf.parent[pRoot] = qRoot
+	} else {
+		uf.parent[qRoot] = pRoot
+	}
+	if pRank == qRank {
+		uf.rank[pRoot]++
+	}
+}
+
+func (uf *unionFind) connected(p, q int) bool {
+	return uf.find(p) == uf.find(q)
+}
+
+func newUnionFind(size int) *unionFind {
+	parent := make([]int, size)
+	rank := make([]byte, size)
+	for i := 0; i < size; i++ {
+		parent[i] = i
+		rank[i] = 0
+	}
+	return &unionFind{
+		parent: parent,
+		rank:   rank,
+	}
+}
+
 // MinimumSpanningTreeKruskal computes minimum spanning tree for a graph using Kruskal's algorithm.
 func MinimumSpanningTreeKruskal(graph EdgeWeightedGraph) EdgeWeightedGraph {
 	return nil
