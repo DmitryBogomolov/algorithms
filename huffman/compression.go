8fd5c7dc7cd32946725644fa02ea1914a56be46f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2020-10-06 13:31:24 +0300

    Add draft compression

---
+++ huffman/compression.go
@@ -0,0 +1,102 @@
+package huffman
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type node struct {
+	ch          byte
+	freq        int
+	left, right *node
+}
+
+type nodesPriorityQueue []*node
+
+func (pq nodesPriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq nodesPriorityQueue) Less(i, j int) bool {
+	return pq[i].freq < pq[j].freq
+}
+
+func (pq nodesPriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *nodesPriorityQueue) Push(item interface{}) {
+	nodes := *pq
+	node := item.(*node)
+	*pq = append(nodes, node)
+}
+
+func (pq *nodesPriorityQueue) Pop() interface{} {
+	nodes := *pq
+	len := len(nodes)
+	node := nodes[len-1]
+	nodes[len-1] = nil
+	*pq = nodes[0 : len-1]
+	return node
+}
+
+func collectFrequencies(data []byte) map[byte]int {
+	frequencies := make(map[byte]int)
+	for _, ch := range data {
+		frequencies[ch]++
+	}
+	return frequencies
+}
+
+func buildTrie(frequencies map[byte]int) *node {
+	nodes := make([]*node, len(frequencies), len(frequencies))
+	i := 0
+	for ch, freq := range frequencies {
+		nodes[i] = &node{ch: ch, freq: freq}
+		i++
+	}
+
+	queue := nodesPriorityQueue(nodes)
+	heap.Init(&queue)
+	for queue.Len() > 1 {
+		left := heap.Pop(&queue).(*node)
+		right := heap.Pop(&queue).(*node)
+		node := node{freq: left.freq + right.freq, left: left, right: right}
+		heap.Push(&queue, &node)
+	}
+	return heap.Pop(&queue).(*node)
+}
+
+func walkTrie(node *node, table map[byte]string, value string) {
+	if node.left == nil && node.right == nil {
+		table[node.ch] = value
+	} else {
+		walkTrie(node.left, table, value+"0")
+		walkTrie(node.right, table, value+"1")
+	}
+}
+
+func buildTable(root *node) map[byte]string {
+	table := make(map[byte]string)
+	walkTrie(root, table, "")
+	return table
+}
+
+func compressCore(data []byte, table map[byte]string) string {
+	result := ""
+	for _, ch := range data {
+		coded := table[ch]
+		result += coded
+	}
+	return result
+}
+
+// Compress compresses *data*.
+// https://algs4.cs.princeton.edu/55compression/Huffman.java.html
+func Compress(data []byte) {
+	frequencies := collectFrequencies(data)
+	root := buildTrie(frequencies)
+	table := buildTable(root)
+	compressed := compressCore(data, table)
+	fmt.Printf("compressed: %d, original: %d\n", len(compressed), len(data)*8)
+}
---
+++ huffman/compression_test.go
@@ -0,0 +1,7 @@
+package huffman
+
+import "testing"
+
+func TestCompress(t *testing.T) {
+	Compress([]byte("it was the best of times it was the worst of times"))
+}
