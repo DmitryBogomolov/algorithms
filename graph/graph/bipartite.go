89b740db2da96950a2e9f99bae9ac6651a4d0ea3
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-10-10 21:48:32 +0300

    Refactor bipartite

--- graph/graph/bipartite.go
+++ graph/graph/bipartite.go
@@ -1,11 +1,13 @@
 package graph
 
-func isBipartiteCore(marked []bool, colors []bool, graph Graph, vertexID int) bool {
+func isBipartiteCore(marked []bool, colors []bool, graph Graph, vertexID int, parentColor bool) bool {
 	marked[vertexID] = true
+	color := !parentColor
+	colors[vertexID] = color
 	for _, adjacentVertexID := range graph.AdjacentVertices(vertexID) {
 		if !marked[adjacentVertexID] {
 			colors[adjacentVertexID] = !colors[vertexID]
-			if !isBipartiteCore(marked, colors, graph, adjacentVertexID) {
+			if !isBipartiteCore(marked, colors, graph, adjacentVertexID, color) {
 				return false
 			}
 		} else if colors[adjacentVertexID] == colors[vertexID] {
@@ -15,15 +17,16 @@ func isBipartiteCore(marked []bool, colors []bool, graph Graph, vertexID int) bo
 	return true
 }
 
-// IsBipartite tells if graph is two-colorable. Uses depth-first search.
+// IsBipartite tells if graph is two-colorable - such that each no edge connects vertices of the same color.
+// In a bipartite graph vertices can be divided into two sets such that all edges connect a vertex
+// in one set with a vertex in other set.
 // https://algs4.cs.princeton.edu/41graph/Bipartite.java.html
 func IsBipartite(graph Graph) bool {
-	numVertices := graph.NumVertices()
-	marked := make([]bool, numVertices)
-	colors := make([]bool, numVertices)
-	for vertexID := 0; vertexID < numVertices; vertexID++ {
+	marked := make([]bool, graph.NumVertices())
+	colors := make([]bool, graph.NumVertices())
+	for vertexID := 0; vertexID < graph.NumVertices(); vertexID++ {
 		if !marked[vertexID] {
-			if !isBipartiteCore(marked, colors, graph, vertexID) {
+			if !isBipartiteCore(marked, colors, graph, vertexID, true) {
 				return false
 			}
 		}
