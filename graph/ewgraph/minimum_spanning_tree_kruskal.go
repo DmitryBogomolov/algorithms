66603c2270fbf6a6327efa4cbef122cfcb43de6d
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2021-10-15 12:21:19 +0300

    Use priorityqueue package

--- graph/ewgraph/minimum_spanning_tree_kruskal.go
+++ graph/ewgraph/minimum_spanning_tree_kruskal.go
@@ -2,28 +2,36 @@ package ewgraph
 
 import (
 	"algorithms/graph/graph"
+	"algorithms/priorityqueue"
 	"algorithms/unionfind"
 )
+
+type _EdgesQueueItem struct {
+	edge   graph.Edge
+	weight float64
+}
 
 // BuildMinimumSpanningTreeKruskal computes minimum spanning tree using Kruskal's algorithm.
 // https://algs4.cs.princeton.edu/43mst/KruskalMST.java.html
 func BuildMinimumSpanningTreeKruskal(ewgraph EdgeWeightedGraph) EdgeWeightedGraph {
-	edgesPriorityQueue := newEdgesPriorityQueue()
+	edgesPriorityQueue := priorityqueue.New(func(lhs, rhs interface{}) bool {
+		return lhs.(_EdgesQueueItem).weight < rhs.(_EdgesQueueItem).weight
+	})
 	allWeights := AllGraphWeights(ewgraph)
 	for i, edge := range graph.AllGraphEdges(ewgraph) {
-		edgesPriorityQueue.pushEdge(edge, allWeights[i])
+		edgesPriorityQueue.Insert(_EdgesQueueItem{edge, allWeights[i]})
 	}
 	numVertices := ewgraph.NumVertices()
 	uf := unionfind.New(numVertices)
 	adjacency := make([][]int, numVertices)
 	weights := make([][]float64, numVertices)
 	numEdges := 0
-	for edgesPriorityQueue.Len() > 0 {
-		edge, weight := edgesPriorityQueue.popEdge()
-		vertexID1, vertexID2 := edge.Vertex1(), edge.Vertex2()
+	for edgesPriorityQueue.Size() > 0 {
+		queueItem := edgesPriorityQueue.Remove().(_EdgesQueueItem)
+		vertexID1, vertexID2 := queueItem.edge.Vertex1(), queueItem.edge.Vertex2()
 		if !uf.Connected(vertexID1, vertexID2) {
 			uf.Union(vertexID1, vertexID2)
-			addWeightedEdge(adjacency, weights, vertexID1, vertexID2, weight)
+			addWeightedEdge(adjacency, weights, vertexID1, vertexID2, queueItem.weight)
 			numEdges++
 		}
 	}
