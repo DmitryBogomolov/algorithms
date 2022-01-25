cea57aba6c9bde29289925a23e6026bcb3a557f2
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-01-25 11:25:12 +0300

    Update graph_impl_test

--- graph/graph/graph_impl_test.go
+++ graph/graph/graph_impl_test.go
@@ -1,13 +1,14 @@
-package graph
+package graph_test
 
 import (
 	"testing"
 
+	. "github.com/DmitryBogomolov/algorithms/graph/graph"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewImplGraph(t *testing.T) {
-	target := NewImplGraph(5, 3, [][]int{
+	gr := NewImplGraph(5, 3, [][]int{
 		{1, 3},
 		{0, 2},
 		{1},
@@ -15,11 +16,11 @@ func TestNewImplGraph(t *testing.T) {
 		nil,
 	})
 
-	assert.Equal(t, target.NumVertices(), 5, "vertices")
-	assert.Equal(t, target.NumEdges(), 3, "edges")
-	assert.Equal(t, target.AdjacentVertices(0), []int{1, 3}, "vertex 0")
-	assert.Equal(t, target.AdjacentVertices(1), []int{0, 2}, "vertex 1")
-	assert.Equal(t, target.AdjacentVertices(2), []int{1}, "vertex 2")
-	assert.Equal(t, target.AdjacentVertices(3), []int{0}, "vertex 3")
-	assert.Equal(t, target.AdjacentVertices(4), []int(nil), "vertex 4")
+	assert.Equal(t, gr.NumVertices(), 5, "vertices")
+	assert.Equal(t, gr.NumEdges(), 3, "edges")
+	assert.Equal(t, gr.AdjacentVertices(0), []int{1, 3}, "vertex 0")
+	assert.Equal(t, gr.AdjacentVertices(1), []int{0, 2}, "vertex 1")
+	assert.Equal(t, gr.AdjacentVertices(2), []int{1}, "vertex 2")
+	assert.Equal(t, gr.AdjacentVertices(3), []int{0}, "vertex 3")
+	assert.Equal(t, gr.AdjacentVertices(4), []int(nil), "vertex 4")
 }
