c3ec10ed39a93ecf1c347e15cfa31d74d081d9fa
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2022-01-27 10:01:01 +0300

    Add two simple tests

--- huffman/compress_expand_test.go
+++ huffman/compress_expand_test.go
@@ -8,6 +8,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCompress(t *testing.T) {
+	data, err := Compress([]byte("Hello World"))
+	assert.Equal(t, nil, err)
+	assert.Equal(
+		t,
+		[]byte{0x7c, 0x13, 0x64, 0x99, 0x6c, 0x24, 0x8b, 0xe4, 0x55, 0x39, 0xb, 0x00, 0x00, 0x00, 0xeb, 0x42, 0xc7, 0x37},
+		data,
+	)
+}
+
+func TestExpand(t *testing.T) {
+	data, err := Expand(
+		[]byte{0x7c, 0x13, 0x64, 0x99, 0x6c, 0x24, 0x8b, 0xe4, 0x55, 0x39, 0xb, 0x00, 0x00, 0x00, 0xeb, 0x42, 0xc7, 0x37},
+	)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, []byte("Hello World"), data)
+}
+
 func TestCompressExpand(t *testing.T) {
 	sample := []byte("it was the best of times it was the worst of times")
 	compressed, compressErr := Compress(sample)
