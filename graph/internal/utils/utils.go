443812d21d6602973b0098559812bd5708be9703
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-01-18 01:16:35 +0300

    Make graph/utils generic

--- graph/internal/utils/utils.go
+++ graph/internal/utils/utils.go
@@ -1,22 +1,24 @@
 package utils
 
+import "golang.org/x/exp/constraints"
+
 // ResetList sets all list elements to -1.
-func ResetList(list []int) {
+func ResetList[T constraints.Signed](list []T) {
 	for i := range list {
 		list[i] = -1
 	}
 }
 
 // ReverseList reverses list.
-func ReverseList(list []int) {
+func ReverseList[T any](list []T) {
 	for i, j := 0, len(list)-1; i < j; i, j = i+1, j-1 {
 		list[i], list[j] = list[j], list[i]
 	}
 }
 
 // SumList returns sum of list items.
-func SumList(list []float64) float64 {
-	sum := 0.0
+func SumList[T constraints.Float](list []T) T {
+	sum := T(0.0)
 	for _, item := range list {
 		sum += item
 	}
@@ -24,8 +26,8 @@ func SumList(list []float64) float64 {
 }
 
 // Min returns minimal element.
-func Min(lhs int, rhs int) int {
-	var ret int
+func Min[T constraints.Ordered](lhs, rhs T) T {
+	var ret T
 	if lhs < rhs {
 		ret = lhs
 	} else {
